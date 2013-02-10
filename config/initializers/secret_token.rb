29f6c7ecd2613bab0b386f012ba047ad641f5270
Chandan Jog <jog.chandan@gmail.com>
2013-02-10 12:18:46 +0000

    Chandan|Creating bare bones project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TypeMe::Application.config.secret_token = '42bf66d7af215363a04ee723e5e217821dc89ed914a42bebedcdf1c6927a9775062dc53c57d9cc1b099183ba7d7e552c0e0007c1f984df280997bbe258d586b3'
