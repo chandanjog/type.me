4c25f45b759324c2d814ee9b4c71e943d4abf847
Chandan Jog <jog.chandan@gmail.com>
2013-05-27 22:07:21 +0100

    Trying to get things working on heroku with sqlite

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ TypeMe::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  config.cache_store = :null_store
+  #config.cache_store = :null_store
 end
 
 ENV["REDISTOGO_URL"] = "redis://redistogo:f9bfe3d00e9b6f55c71601f24cdc021c@cod.redistogo.com:9067/"
